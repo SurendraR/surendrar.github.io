298ed006c793b8220e906ba8745df5872eae721e
SurendraR <srayapati28@gmail.com>
2025-03-09 19:48:22 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/surendrar.github.io/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 });
